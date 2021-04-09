fba556f72ece17fa8dd3a872a7ee9f0b041320cd
Jawad <jawad@vortechs.io>
2021-04-09 17:33:26 +0500

    extra files removed some minor changes

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -65,7 +65,7 @@ export default function Admin({ ...rest }) {
     setMobileOpen(!mobileOpen);
   };
   const getRoute = () => {
-    return window.location.pathname !== "/admin/maps";
+    return window.location.pathname !== "/admin/dashboard";
   };
   const resizeFunction = () => {
     if (window.innerWidth >= 960) {
@@ -94,7 +94,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim"}
+        logoText={"Jawad"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
